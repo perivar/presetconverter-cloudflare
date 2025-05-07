2f6f825035aa50ba40a821c864fa0ee602dcce6c
Per Ivar Nerseth <perivar@nerseth.com>
2025-05-07 18:54:37 +0200

    remove unused reflect-metadata package

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ const untranspiledModulePatterns = [""];
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
-  setupFilesAfterEnv: ["reflect-metadata"],
+  setupFilesAfterEnv: [],
   roots: ["app"],
   transform: {
     "\\.[jt]sx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
